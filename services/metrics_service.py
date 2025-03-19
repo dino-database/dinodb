388b8c01cc585475955c30a8afcf4e196d716a1b
Deyan Sirakov <deyan.sirakov2006@abv.bg>
2025-03-20 01:13:41 +0200

    fixed endpoint

--- services/metrics_service.py
+++ services/metrics_service.py
@@ -5,30 +5,37 @@ from pathlib import Path
 from utility import config
 
 class MetricsService:
-    async def get_metrics(self):
+    def get_metrics(self):
+        return { 
+                "database_metrics": "true", 
+                "system_metrics": "true" 
+            }
+        
+        
+        
         # Database Metrics
         #wal_size = os.path.getsize("wal.log") if os.path.exists("wal.log") else 0
         #memtable_size = len(db.sl)  # Assuming 'db.sl' is your MemTable (SkipList)
         #sstable_count = len([f for f in Path(".").glob("sstable_*.json")])
         
         # System Metrics
-        memory_info = psutil.virtual_memory()
-        cpu_usage = psutil.cpu_percent(interval=1)
-        disk_info = psutil.disk_usage("/")
+        # memory_info = psutil.virtual_memory()
+        # cpu_usage = psutil.cpu_percent(interval=1)
+        # disk_info = psutil.disk_usage("/")
 
-        return {
+        # return {
             # "database_metrics": {
             #     "total_entries": memtable_size + sum(len(db.sstable.read_sstable(f"sstable_{i}.json")) for i in range(sstable_count)),
             #     "wal_file_size_bytes": wal_size,
             #     "memtable_size": memtable_size,
             #     "sstable_file_count": sstable_count,
             # },
-            "system_metrics": {
-                "memory_used_mb": memory_info.used / (1024 ** 2),
-                "memory_available_mb": memory_info.available / (1024 ** 2),
-                "cpu_usage_percent": cpu_usage,
-                "disk_used_gb": disk_info.used / (1024 ** 3),
-                "disk_free_gb": disk_info.free / (1024 ** 3),
-                "uptime_seconds": round(time.time() - config.db_start_time, 2)
-            }
-        }
+            # "system_metrics": {
+            #     "memory_used_mb": memory_info.used / (1024 ** 2),
+            #     "memory_available_mb": memory_info.available / (1024 ** 2),
+            #     "cpu_usage_percent": cpu_usage,
+            #     "disk_used_gb": disk_info.used / (1024 ** 3),
+            #     "disk_free_gb": disk_info.free / (1024 ** 3),
+            #     "uptime_seconds": round(time.time() - config.db_start_time, 2)
+            # }
+        # }
